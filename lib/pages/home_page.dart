cc739e6037ed27b88a2b3a7868462cd81095d855
Belmin Mulahusic <belminmulahusic@Belmins-MacBook-Air.local>
2025-04-16 00:40:32 +0200

    started building function to estimate task duration

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'todo_tile.dart';
 
-class TodoHomePage extends StatelessWidget {
+class TodoHomePage extends StatefulWidget {
   const TodoHomePage({super.key});
+
+  @override
+  State<TodoHomePage> createState() => _TodoHomePage();
+}
+
+class _TodoHomePage extends State<TodoHomePage> {
+  final List tasks = [
+    ['Do homework in english', false, 20],
+    ['Write french essay', false, 60],
+    ['Cook meal for tomorrow', false, 30],
+    ['Clean your room', false, 40],
+    ['Go for a walk', false, 90],
+    ['Study for DS', false, 60],
+    ['Build an app', false, 120],
+    ['Study chapter 3, 4, 5, 6 in maths', false, 20],
+    [
+      'Write a long ass line to see how far the box will go, Write a long ass line to see how far the box will go',
+      false,
+      5,
+    ],
+    ['Whatup', false, 15],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      tasks[index][1] = !tasks[index][1];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,28 +47,25 @@ class TodoHomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
             shadows: [
               Shadow(
-                color: Color.fromARGB(255, 17, 70, 98),
-                offset: Offset(3.0, 1.0),
-                blurRadius: 3.0,
+                color: Color.fromARGB(255, 165, 165, 165),
+                offset: Offset(5, 0),
+                blurRadius: 0,
               ),
             ],
             letterSpacing: 2,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const Divider(
-            color: Color.fromARGB(255, 72, 173, 227),
-            thickness: 10,
-            height: 10,
-          ),
-          TodoTile(
-            taskName: 'Do my homework in english',
-            isDone: false,
-            onChanged: (bool? value) => {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: tasks[index][0],
+            isDone: tasks[index][1],
+            minutes: tasks[index][2],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
--- lib/pages/todo_tile.dart
+++ lib/pages/todo_tile.dart
@@ -3,22 +3,23 @@ import 'package:flutter/material.dart';
 class TodoTile extends StatelessWidget {
   final String taskName;
   final bool isDone;
+  final int minutes;
   final Function(bool?) onChanged;
 
   const TodoTile({
     super.key,
     required this.taskName,
     required this.isDone,
+    required this.minutes,
     required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
       child: Container(
         width: 400,
-        height: 60,
         decoration: BoxDecoration(
           color: Color.fromARGB(255, 79, 193, 255),
           borderRadius: BorderRadius.circular(20),
@@ -28,17 +29,44 @@ class TodoTile extends StatelessWidget {
             Checkbox(
               value: isDone,
               onChanged: onChanged,
-              activeColor: Colors.white,
-              checkColor: Colors.black,
+              activeColor: const Color.fromARGB(255, 187, 230, 255),
+              checkColor: const Color.fromARGB(255, 0, 44, 78),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromARGB(255, 79, 193, 255),
+                  ),
+                  child: Text(
+                    taskName,
+                    style: const TextStyle(fontSize: 18),
+                    overflow: TextOverflow.visible,
+                    maxLines: null,
+                  ),
+                ),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.only(right: 12.0),
               child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: const Color.fromARGB(255, 79, 193, 255),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 4,
                 ),
-                child: Text(taskName, style: const TextStyle(fontSize: 18)),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 12, 170, 20),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  minutes.toString(),
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ],
